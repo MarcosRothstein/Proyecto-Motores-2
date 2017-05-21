072042b30a59be43fd23c22ada8b10e1b47fd866
Rothstein <rothsteinmarcos@gmail.com>
2017-05-21 10:38:19 -0300

    Sistema de nodos

---
+++ Proyecto Motores 2/Assets/Scripts/NodeEditor.cs
@@ -0,0 +1,202 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class NodeEditor : EditorWindow
+{
+    ///> <
+    private List<Nodo> windows = new List<Nodo>();
+    private Nodo SelectedNode;
+    private bool makeTransitionMode = false;
+    private Vector2 mousePos;
+
+    [MenuItem("Shot Editor/Editor de Nodos")]
+    static void showEditor()
+    {
+        NodeEditor editor = EditorWindow.GetWindow<NodeEditor>();
+    }
+    void OnGUI()
+    {
+        Event e = Event.current;
+        mousePos = e.mousePosition;
+        if (e.button == 1 && !makeTransitionMode)
+        {
+            if (e.type == EventType.MouseDown)
+            {
+                bool clickedOnWindow = false;
+                int selectedIndex = -1;
+                for (int i = 0; i < windows.Count; i++)
+                {
+                    if (windows[i].windowRect.Contains(mousePos))
+                    {
+                        selectedIndex = i;
+                        clickedOnWindow = true;
+                        break;
+                    }
+                }
+                if (!clickedOnWindow)
+                {
+                    GenericMenu menu = new GenericMenu();
+                    menu.AddItem(new GUIContent("agregar Nodo"), false, contextCallback, "Nodo");
+                    menu.ShowAsContext();
+                    e.Use();
+                }
+                else
+                {
+                    GenericMenu menu = new GenericMenu();
+                    menu.AddItem(new GUIContent("Likear Nodo"), false, contextCallback, "makeTransition");
+                    menu.AddSeparator("");
+                    menu.AddItem(new GUIContent("Eliminar Nodo"), false, contextCallback, "deleteNode");
+                    menu.ShowAsContext();
+                    e.Use();
+                }
+            }
+        }
+        else if (e.button == 0 && e.type == EventType.MouseDown && makeTransitionMode)
+        {
+            bool clickedOnWindow = false;
+            int selectedIndex = -1;
+            for (int i = 0; i < windows.Count; i++)
+            {
+                if (windows[i].windowRect.Contains(mousePos))
+                {
+                    selectedIndex = i;
+                    clickedOnWindow = true;
+                    break;
+                }
+            }
+            if (clickedOnWindow && !windows[selectedIndex].Equals(SelectedNode))
+            {
+                windows[selectedIndex].setInput((Nodo)SelectedNode, mousePos);
+                makeTransitionMode = false;
+                SelectedNode = null;
+            }
+            if (!clickedOnWindow)
+            {
+                makeTransitionMode = false;
+                SelectedNode = null;
+            }
+            e.Use();
+        }
+        else if (e.button == 0 && e.type == EventType.MouseDown && !makeTransitionMode)
+        {
+            bool clickedOnWindow = false;
+            int selextIndex = -1;
+            for (int i = 0; i < windows.Count; i++)
+            {
+                if (windows[i].windowRect.Contains(mousePos))
+                {
+                    selextIndex = i;
+                    clickedOnWindow = true;
+                    break;
+                }
+            }
+            if (clickedOnWindow)
+            {
+                Nodo nodeToChange = windows[selextIndex].clickedOnInput(mousePos);
+                if (nodeToChange != null)
+                {
+                    SelectedNode = nodeToChange;
+                    makeTransitionMode = true;
+                }
+;
+            }
+        }
+        if (makeTransitionMode && SelectedNode != null)
+        {
+            Rect mouserect = new Rect(e.mousePosition.x, e.mousePosition.y, 10, 10);
+            DrawNodeCurve(SelectedNode.windowRect, mouserect);
+            Repaint();
+        }
+        foreach (Nodo nodo in windows)
+        {
+            nodo.drawCurves();
+        }
+        BeginWindows();
+        for (int i = 0; i < windows.Count; i++)
+        {
+            windows[i].windowRect = GUI.Window(i, windows[i].windowRect, drawNodeWindow, windows[i].windowTitle);
+        }
+        EndWindows();
+
+    }
+    void contextCallback(object objeto)
+    {
+        string callbk = objeto.ToString();
+        if (callbk.Equals("Nodo"))
+        {
+            Nodo Inodo = new Nodo();
+            Inodo.windowRect = new Rect(mousePos.x, mousePos.y, 200, 150);
+            windows.Add(Inodo);
+        }       
+       
+        else if (callbk.Equals("makeTransition"))
+        {
+            bool clickedOnWindow = false;
+            int selextIndex = -1;
+            for (int i = 0; i < windows.Count; i++)
+            {
+                if (windows[i].windowRect.Contains(mousePos))
+                {
+                    selextIndex = i;
+                    clickedOnWindow = true;
+                    break;
+                }
+            }
+            if (clickedOnWindow)
+            {
+                SelectedNode = windows[selextIndex];
+                makeTransitionMode = true;
+            }
+        }
+        else if (callbk.Equals("deleteNode"))
+        {
+            bool clickedOnWindow = false;
+            int selextIndex = -1;
+            for (int i = 0; i < windows.Count; i++)
+            {
+                if (windows[i].windowRect.Contains(mousePos))
+                {
+                    selextIndex = i;
+                    clickedOnWindow = true;
+                    break;
+                }
+            }
+            if (clickedOnWindow)
+            {
+                Nodo selNode = windows[selextIndex];
+                windows.RemoveAt(selextIndex);
+                foreach (Nodo nodo in windows)
+                {
+                    if (SelectedNode != null)
+                    {
+                        nodo.deletNode(SelectedNode);
+                    }
+                }
+            }
+        }
+
+
+    }
+    void drawNodeWindow(int id)
+    {
+        windows[id].DrawWindow();
+        GUI.DragWindow();
+    }
+    public static void DrawNodeCurve(Rect start, Rect end)
+    {
+        Vector3 startPos = new Vector3(start.x + start.width / 2, start.y + start.height / 2, 0);
+        Vector3 endPos = new Vector3(end.x + end.width / 2, end.y + end.height / 2, 0);
+        Vector3 starTan = startPos + Vector3.right * 50;
+        Vector3 endTan = endPos + Vector3.left * 50;
+
+        for (int i = 0; i < 3; i++)
+        {
+            Handles.DrawBezier(startPos, endPos, starTan, endTan, Color.black, null, (i + 1) * 5);
+        }
+        Handles.DrawBezier(startPos, endPos, starTan, endTan, Color.black, null, 1);
+    }
+
+}
+
---
+++ Proyecto Motores 2/Assets/Scripts/Nodo.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class Nodo : ScriptableObject {
+    public Rect windowRect;
+    public string windowTitle = "";
+    private Nodo Ant;
+    private Rect AntRect;
+    private Efectos efecto;
+    public enum Efectos
+    {
+        efecto1,
+        efecto2,
+        efecto3,
+        
+    }
+    public Nodo()
+    {
+        windowTitle = "Nombre Nodo";       
+    }
+
+
+    public  void DrawWindow()
+    {
+        windowTitle = EditorGUILayout.TextField("Nombre", windowTitle);
+        Event e = Event.current;
+        GUILayout.Label("Anterior" );
+        if (e.type == EventType.repaint)
+        {
+            AntRect = GUILayoutUtility.GetLastRect();
+        }
+        GUILayout.Label("Selecciona un efecto");
+
+        efecto = (Efectos)EditorGUILayout.EnumPopup(efecto);
+    }
+    public  void setInput(Nodo input, Vector2 clickPos)
+    {
+        clickPos.x -= windowRect.x;
+        clickPos.y -= windowRect.y;
+        if (AntRect.Contains(clickPos))
+        {
+            Ant = input;
+        }
+       
+    }
+    public  void drawCurves()
+    {
+        if (Ant != null)
+        {
+            Rect rect = windowRect;
+            rect.x += AntRect.x;
+            rect.y += AntRect.y + AntRect.height / 2;
+            rect.width = 1;
+            rect.height = 1;
+            NodeEditor.DrawNodeCurve(Ant.windowRect, rect);
+        }
+    }
+    public  void getResult()
+    {
+
+        switch (efecto)
+        {
+            case Efectos.efecto1:
+                //aplicar efecto1 a camara
+                break;
+            case Efectos.efecto2:
+                //aplicar efecto2 a camara
+                break;
+            case Efectos.efecto3:
+                //aplicar efecto3 a camara
+                break;
+        }
+    }
+    public  Nodo clickedOnInput(Vector2 pos)
+    {
+        Nodo retval = null;
+        pos.x -= windowRect.x;
+        pos.y -= windowRect.y;
+        if (AntRect.Contains(pos))
+        {
+            retval = Ant;
+            Ant = null;
+        }       
+        return retval;
+    }
+
+    public  void deletNode(Nodo node)
+    {
+        if (node.Equals(Ant) && Ant != null)
+        {
+            Ant = null;
+        }
+       
+    }
+}
