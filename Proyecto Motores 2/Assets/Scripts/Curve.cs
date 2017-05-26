c833153ad89454b6d87ad0b5a15bcfaa23ef014d
diego_yyz <diego_yyz@hotmail.com>
2017-05-26 19:54:25 -0300

    Sistema de nodos con curvas

---
+++ Proyecto Motores 2/Assets/Scripts/Curve.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Curve : MonoBehaviour {
+
+    public Nodo[] points;
+
+    public void Reset()
+    {
+        for (int i = 0; i < points.Length; i++)
+        {
+            points[i].transform.position = new Vector3(i,0f,0f);
+        }
+    }
+    public Vector3 GetPoint(float t)
+    {
+        return transform.TransformPoint(Bezier.GetPoint(points[0].transform.position, points[1].transform.position, points[2].transform.position, t));
+    }
+   
+}
