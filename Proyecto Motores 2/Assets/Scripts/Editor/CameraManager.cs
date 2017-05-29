40ea50a5e6c129a702bb6768b37adc606b727936
Callipygian <necrossx@gmail.com>
2017-05-29 00:32:26 -0300

    Camera manager

---
+++ Proyecto Motores 2/Assets/Scripts/Editor/CameraManager.cs
@@ -0,0 +1,44 @@
+﻿	using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+public class CameraManager : EditorWindow {
+
+	private Camera _camera;
+	private RenderTexture _preview;
+[MenuItem("Window/Shot editor/Open camera manager")]
+
+	static void CreateWindow()
+	{
+		GetWindow(typeof(CameraManager)).Show();
+
+	}
+		
+	private void GetCameraPreview()
+	{
+		_preview = new RenderTexture(512,512,0);	
+	}
+
+
+	void OnGUI()
+	{
+		_camera = (Camera)EditorGUILayout.ObjectField("Cámara: ", _camera, typeof(Camera),true);
+		if(_camera != null)
+		{
+			GetCameraPreview();
+			_camera.targetTexture = _preview;
+			_camera.Render();
+			_camera.targetTexture = null;
+			if(_preview) GUI.DrawTexture(new Rect(25, 25, 200, 200), _preview, ScaleMode.ScaleToFit, false, 1f);
+		}
+		
+	}
+
+	void OnDestroy()
+	{
+		_preview = null;
+	}
+	 	
+}
