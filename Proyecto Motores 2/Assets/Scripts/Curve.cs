fe555058a7f7bc63f80dac36b6d291809097e47d
diego_yyz <diego_yyz@hotmail.com>
2017-05-28 22:44:09 -0300

    Se agregan curvas y los slots para los nodos, pero falta que instancie los nodos.

--- Proyecto Motores 2/Assets/Scripts/Curve.cs
+++ Proyecto Motores 2/Assets/Scripts/Curve.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Curve : MonoBehaviour {
-
+    public Nodo Prfab;
     public Nodo[] points;
 
     public void Reset()
@@ -13,9 +14,29 @@ public class Curve : MonoBehaviour {
             points[i].transform.position = new Vector3(i,0f,0f);
         }
     }
+    public Vector3 GetDirection(float t)
+    {
+        return GetVelocity(t).normalized;
+    }
+    public void AddCurve()
+    {
+        Nodo point = points[points.Length - 1];
+        Array.Resize(ref points, points.Length + 3);
+
+        point.transform.position += new Vector3 (1f,0f,0f);
+        points[points.Length - 3] = Instantiate<Nodo>(Prfab);
+        point.transform.position += new Vector3(1f, 0f, 0f);
+        points[points.Length - 2] = Instantiate<Nodo>(Prfab);
+        point.transform.position += new Vector3(1f, 0f, 0f);
+        points[points.Length - 1] = Instantiate<Nodo>(Prfab);
+    }
     public Vector3 GetPoint(float t)
     {
-        return transform.TransformPoint(Bezier.GetPoint(points[0].transform.position, points[1].transform.position, points[2].transform.position, t));
+        return transform.TransformPoint(Bezier.GetPoint(points[0].transform.position, points[1].transform.position, points[2].transform.position, points[3].transform.position, t));
     }
-   
+    public Vector3 GetVelocity(float t)
+    {
+        return transform.TransformPoint(Bezier.GetFirstD(points[0].transform.position, points[1].transform.position, points[2].transform.position, points[3].transform.position, t)) -
+            transform.position;
+    }
 }
--- Proyecto Motores 2/Assets/Scripts/Editor/CurveEditor.cs
+++ Proyecto Motores 2/Assets/Scripts/Editor/CurveEditor.cs
@@ -5,10 +5,15 @@ using UnityEditor;
 
 [CustomEditor(typeof(Curve))]
 public class CurveEditor : Editor {
+
+
     private Curve _target;
     private Transform handleTransform;
     private Quaternion handleRotation;
     private const int lineSteps = 10;
+
+    private const float directionScale = 0.5f;
+
 
     private void OnSceneGUI()
     {
@@ -16,23 +21,45 @@ public class CurveEditor : Editor {
         handleTransform = _target.transform;
         handleRotation = Tools.pivotRotation == PivotRotation.Local ?
             handleTransform.rotation : Quaternion.identity;
+
         Vector3 p0 = ShowPoint(0);
-        Vector3 p1 = ShowPoint(1);
-        Vector3 p2 = ShowPoint(2);
+       
+        for (int i = 1; i < _target.points.Length; i += 3)
+        {
+            Vector3 p1 = ShowPoint(i);
+            Vector3 p2 = ShowPoint(i + 1);
+            Vector3 p3 = ShowPoint(i + 2);
 
-        Handles.color = Color.gray;
-        Handles.DrawLine(p0, p1);
-        Handles.DrawLine(p1, p2);
+            Handles.color = Color.gray;
+            Handles.DrawLine(p0, p1);
+            Handles.DrawLine(p2, p3);
 
-        Handles.color = Color.white;
-        Vector3 lineStart = _target.GetPoint(0f);
+            Handles.DrawBezier(p0, p3, p1, p2, Color.white, null, 2f);
+            p0 = p3;
+        }
+        ShowDirections();
+
+    }
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        _target = (Curve)target; if (GUILayout.Button("Add Curve"))
+        {
+            Undo.RecordObject(_target, "Add Curve");
+            _target.AddCurve();
+        }
+    }
+
+    private void ShowDirections()
+    {
+        Handles.color = Color.green;
+        Vector3 point = _target.GetPoint(0f);
+        Handles.DrawLine(point, point + _target.GetDirection(0f) * directionScale);
         for (int i = 1; i <= lineSteps; i++)
         {
-            Vector3 lineEnd = _target.GetPoint(i / (float)lineSteps);
-            Handles.DrawLine(lineStart, lineEnd);
-            lineStart = lineEnd;
+            point = _target.GetPoint(i / (float)lineSteps);
+            Handles.DrawLine(point, point + _target.GetDirection(i / (float)lineSteps) * directionScale);
         }
-
     }
     private Vector3 ShowPoint(int index)
     {
