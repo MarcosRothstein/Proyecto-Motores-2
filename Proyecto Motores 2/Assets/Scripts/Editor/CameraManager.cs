6803a149a03b1e1be2cd8e0ca0966b0463f505bc
Callipygian <necrossx@gmail.com>
2017-06-11 16:33:29 -0300

    Se actualizó el camera manager para mostrar la posición de la camara seleccionada

--- Proyecto Motores 2/Assets/Scripts/Editor/CameraManager.cs
+++ Proyecto Motores 2/Assets/Scripts/Editor/CameraManager.cs
@@ -24,21 +24,29 @@ public class CameraManager : EditorWindow {
 
 	void OnGUI()
 	{
-
+		
 		minSize = new Vector2(256, 512);
-
+		EditorGUILayout.BeginVertical();
 		_camera = (Camera)EditorGUILayout.ObjectField("Cámara: ", _camera, typeof(Camera),true);
+
+
 
 
 		EditorGUILayout.BeginHorizontal();
 
 		GUILayout.Button("←");
 		GUILayout.Button("Update");
-		GUILayout.Button(	"→");
+		GUILayout.Button("→");
 
 
 		EditorGUILayout.EndHorizontal();
+		EditorGUILayout.EndVertical();
 
+		//_camera.transform.position= EditorGUILayout.Vector3Field("Camera Position ", _camera.transform.position);
+
+	
+
+	
 
 		if(_camera != null)
 		{
@@ -46,18 +54,26 @@ public class CameraManager : EditorWindow {
 			_camera.targetTexture = _preview;
 			_camera.Render();
 			_camera.targetTexture = null;
-			if(_preview) GUI.DrawTexture(new Rect(0 , 50, 270, 256), _preview, ScaleMode.ScaleToFit, false, 1f);
+			if(_preview) GUI.DrawTexture(GUILayoutUtility.GetRect(256,256), _preview, ScaleMode.ScaleToFit, false, 1f);
+
+			EditorGUILayout.Vector3Field("Camera Position", _camera.transform.position);
+			
+			//EditorGUI.Vector3Field(GUILayoutUtility.GetRect(64,64), "Camera position:", _camera.transform.position);
+
 		}
 		else 
 		{
-			GUILayoutUtility.GetRect(0, 25, 64, 64);
-			EditorGUILayout.LabelField("El preview aparecerá aquí");
-			GUILayoutUtility.GetRect(0, 25, 64, 64);
+
+			GUI.DrawTexture(GUILayoutUtility.GetRect(256,256),(Texture2D)Resources.Load("Textures/Checker"),ScaleMode.ScaleToFit,true,1f);
+//			GUILayoutUtility.GetRect(128,128);
+//			EditorGUILayout.LabelField("El preview aparecerá aquí");
+//			GUILayoutUtility.GetRect(128,128);
 
 		}
+			
 
 
-
+	
 	}
 
 
